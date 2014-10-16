c2ab8ec6b5f4cc43889b4eff334f65817b03efa3
Guillaume Clement <gclement@baobob.org>
2014-10-17 01:28:33 +0200

    Allow to create a Target with a location

--- bloblines/src/org/bloblines/data/map/Target.java
+++ bloblines/src/org/bloblines/data/map/Target.java
@@ -5,10 +5,16 @@ import java.util.Set;
 /**
  * This is a target possible inside a location.
  * 
- * Targets contain both the destination location as well as the different
- * required data.
+ * Targets contain both the destination location as well as the different required data.
  */
 public class Target {
 	public Location destination;
 	public Set<Requirement> requirements;
+
+	public Target() {
+	}
+
+	public Target(Location location) {
+		destination = location;
+	}
 }
